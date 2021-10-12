8de5789e1d787f1f7e5fdc3ebf56a8d74e904c36
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-10-11 23:21:05 -0500

    Base files for Sequential Files

---
+++ SequentialFile/sequential_file.h
@@ -0,0 +1,14 @@
+#ifndef SEQUENTIAL_FILE_H
+#define SEQUENTIAL_FILE_H
+
+#include <bits/stdc++.h>
+
+template <class Record, class Key>
+class SequentialFile{
+private:
+    
+public:
+
+};
+
+#endif //
