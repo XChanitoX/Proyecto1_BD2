229cbe86490125347b4237a68e119124ccf842f8
Claudio Echarre <48997991+cecharre@users.noreply.github.com>
2021-10-19 14:07:22 -0500

    update Bucket

---
+++ Hash/Bucket.cpp
@@ -0,0 +1,66 @@
+#include "Bucket.h"
+
+template<class Record, class Key>
+Bucket<Record, Key>::Bucket():nextBucket(-1), count(0), nextDel(0) {}
+
+
+template<class Record, class Key>
+Bucket<Record, Key>::Bucket(int _nextBucket_):nextBucket(_nextBucket_), count(0), nextDel(0) {}
+
+
+template<class Record, class Key>
+int Bucket<Record, Key>::getNextBucket() const {
+    return nextBucket;
+}
+
+template<class Record, class Key>
+int Bucket<Record, Key>::getNextDel(){
+    return nextDel;
+}
+
+template<class Record, class Key>
+void Bucket<Record, Key>::setNextBucket(int nextBucket) {
+    Bucket::nextBucket = nextBucket;
+}
+
+template<class Record, class Key>
+void Bucket<Record, Key>::addRecord(Record record){
+
+}
+
+template<class Record, class Key>
+void Bucket<Record, Key>::sortBucket(){
+    sort(arrayRecords, arrayRecords+count, compareRecords());
+}
+
+template<class Record, class Key>
+void  Bucket<Record, Key>::setRecords(vector<Record> vector){
+    count = 0;
+    for(count = 0; count<vector.size(); count++)
+        arrayRecords[count]=vector[count];
+}
+
+template<class Record, class Key>
+bool Bucket<Record, Key>::isFull(){
+    return count==BLOCKFACTOR;
+}
+
+template<class Record, class Key>
+bool Bucket<Record, Key>::isEmpty(){
+    return count==0;
+}
+
+template<class Record, class Key>
+bool Bucket<Record, Key>::compareRecords(Record &recordA, Record &recordB) {
+    return recordA<recordB;
+}
+template<class Record, class Key>
+vector<Record> Bucket<Record, Key>::getRecords(){
+    return vector<Record>(arrayRecords, arrayRecords+count);
+}
+
+
+
+
+
+
